464d86edce58f890b90c5a0f53c4e48812b29c8f
wilson Merengo <merengowilson2@gmail.com>
2025-07-30 17:32:47 +0300

    Complete project: all tests passing

---
+++ src/components/Article.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  // BONUS: Minutes to read
+  let emoji = "";
+  if (minutes < 30) {
+    const cups = Math.ceil(minutes / 5);
+    emoji = "☕️".repeat(cups);
+  } else {
+    const bentos = Math.ceil(minutes / 10);
+    emoji = "🍱".repeat(bentos);
+  }
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date}</small>
+      <p>{emoji} {minutes} min read</p>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
